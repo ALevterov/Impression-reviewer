512e8cd1881613304209871b22fc189a9aeb91c4
Alexander Levterov <shuroffrashiday@yandex.ru>
2022-01-17 16:46:00 +0300

    new features added

--- src/addpost.js
+++ src/addpost.js
@@ -1,3 +1,4 @@
+import {Post} from './post'
 export function PostPage() {
   const mainBody = document.querySelector('.main__body')
   let HTML = `
@@ -6,12 +7,47 @@ export function PostPage() {
 				<form>
 					<label for="formFile" class="form-label">Загрузить картинку</label>
   			<input class="form-control" type="file" id="formFile" accept=".png, .jpg, .jpeg, .png, .gif">
-				<label for="exampleFormControlTextarea1" class="form-label">Ваш отзыв о продукте</label>
-				<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+				<label for="textarea" class="form-label">Ваш отзыв о продукте</label>
+				<textarea class="form-control" id="textarea" rows="3"></textarea>
 					<button type="submit" class="btn btn-outline-success" id="create-post">Создать пост<button>
 				</form>
 			</div>
 		</div>
 	`
   mainBody.innerHTML = HTML
+
+  const fileInput = mainBody.querySelector('#formFile')
+  const textInput = mainBody.querySelector('#textarea')	
+  const subBtn = mainBody.querySelector('#create-post')
+  const form = mainBody.querySelector('form')
+  let files=[], text
+  function submitHandler() {
+	if(textInput.value)
+		text = textInput.value
+
+	if(fileInput.files.length !==0)
+		files = Array.from(fileInput.files)
+	
+	files.forEach(file => {
+		if(!file.type.match('image'))
+			return
+		
+		const reader = new FileReader()
+	
+		reader.onload = ev => {
+			console.log('Э');
+			console.log(ev.target.result)
+			const img = document.createElement('img')
+			img.src = ev.target.result
+			console.log(form);
+			// img.src = `${ev.target.result}`
+			form.insertAdjacentElement('afterbegin', img)
+		}
+		reader.readAsDataURL(file)
+	});
+	
+  }
+  subBtn.addEventListener('click', submitHandler)
 }
+
+
--- src/post.js
+++ src/post.js
@@ -1,18 +1,27 @@
 export class Post {
-  static create(post) {
-    fetch(
-      'https://myfirstwebsite-4557d-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-      {
-        method: 'POST',
-        body: JSON.stringify(post),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response)
-      })
+  constructor(img, text) {
+    this.img = img
+    this.text = text
   }
+  
 }
+
+
+// export class Post {
+//   static create(post) {
+//     fetch(
+//       'https://myfirstwebsite-4557d-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+//       {
+//         method: 'POST',
+//         body: JSON.stringify(post),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       }
+//     )
+//       .then((response) => response.json())
+//       .then((response) => {
+//         console.log(response)
+//       })
+//   }
+// }
