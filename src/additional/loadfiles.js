0712aa58c27a6abe2f3335228c5b2418eb1f4128
Alexander Levterov <alexanderlevterov@gmail.com>
2022-01-19 16:58:31 +0300

    new commit

--- src/additional/loadfiles.js
+++ src/additional/loadfiles.js
@@ -1,5 +1,11 @@
 import firebase from 'firebase/compat/app'
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  listAll,
+} from 'firebase/storage'
 import { Post } from '../essenses/post'
 // import { appendChild } from 'domutils'
 // import { getAnalytics } from "firebase/analytics";
@@ -17,37 +23,51 @@ const app = firebase.initializeApp(firebaseConfig)
 
 const storage = getStorage(app)
 
-export function loadContent(section) {
-  const sectionRef = ref(storage, `${section}/`)
+async function loadPostContent(section, post, fileContainer) {
+  const rootRef = ref(storage, `${section}`)
+  const sectionRef = ref(
+    storage,
+    `${section}/${post}//xps-kLfkVa_4aXM-unsplash.jpg`
+  )
+  /// ==============
+  listAll(rootRef).then((rootPrefix) => {
+    rootPrefix.prefixes.forEach((pref) => {
+      listAll(pref).then((post) => {
+        //в post содержатся файлы поста
 
-  // Get the download URL
-  getDownloadURL(sectionRef)
-    .then((url) => {
-      // Insert url into an <img> tag to "download"
-      console.log(url)
+        post.items.forEach((item, index) => {
+          getDownloadURL(item)
+            .then((file) => {
+              //file это скачанный файл из БД
+              fileContainer.push(file)
+            })
+            .catch((error) => {
+              // A full list of error codes is available at
+              // https://firebase.google.com/docs/storage/web/handle-errors
+              switch (error.code) {
+                case 'storage/object-not-found':
+                  console.log('storage/object-not-found')
+                  break
+                case 'storage/unauthorized':
+                  console.log('storage/unauthorized')
+                  // User doesn't have permission to access the object
+                  break
+                case 'storage/canceled':
+                  console.log('storage/canceled')
+                  // User canceled the upload
+                  break
+
+                // ...
+
+                case 'storage/unknown':
+                  console.log('storage/unknown')
+                  // Unknown error occurred, inspect the server response
+                  break
+              }
+            })
+        })
+      })
     })
-    .catch((error) => {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/object-not-found':
-          console.log('storage/object-not-found')
-          break
-        case 'storage/unauthorized':
-          console.log('storage/unauthorized')
-          // User doesn't have permission to access the object
-          break
-        case 'storage/canceled':
-          console.log('storage/canceled')
-          // User canceled the upload
-          break
-
-        // ...
-
-        case 'storage/unknown':
-          console.log('storage/unknown')
-          // Unknown error occurred, inspect the server response
-          break
-      }
-    })
+  })
 }
+module.exports.loadPostContent = loadPostContent
--- src/pages/drinkspage.js
+++ src/pages/drinkspage.js
@@ -1,26 +1,17 @@
 import { Post } from '../essenses/post'
 import { PostPage } from './addpost'
-import { loadContent } from '../additional/loadfiles'
+import { loadPostContent } from '../additional/loadfiles'
 import { setActiveNavLink } from '../additional/setActiveLink'
 export function drinksPageHandler(event) {
   let clientheight = window.innerHeight
   const drinkPosts = ''
   let HTML = `
 	<div class="drinks__posts__wrapper">
-		<div class="drinks-container">
+		<div class="drinks-container" id="post-container">
 			<div class="post">
 				<img src="">
 				<div class="post-text">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis voluptatibus recusandae dolore facilis officiis ex eaque modi suscipit, sequi nesciunt, possimus praesentium nihil. Repellendus reiciendis blanditiis praesentium voluptatem quidem quod, placeat quo recusandae quis dolorem sunt temporibus sequi excepturi mollitia est voluptatum ducimus quas, similique doloribus nobis commodi dolore at qui dicta! Vel, iusto veritatis magnam consequatur autem reprehenderit, asperiores quaerat deserunt adipisci, facilis dignissimos cumque tempore harum ratione rem blanditiis impedit ad pariatur nemo. Sit nostrum corporis magni a aperiam mollitia, maiores laboriosam alias quo praesentium, beatae saepe molestiae debitis cumque quam unde officia sapiente, nisi adipisci tenetur fugiat illo totam! Hic molestiae officiis similique obcaecati. Maxime beatae iste obcaecati repellendus dolore odit expedita veritatis temporibus dolorum, officiis quisquam aspernatur voluptates qui animi consequuntur inventore delectus, accusantium ipsum voluptatem? At sapiente tempora corrupti saepe eum deleniti laboriosam rerum et placeat, nemo, voluptate eaque cum iusto architecto ullam reiciendis eius.
-				</div>
-				<div class="about-author">
-
-				</div>
-			</div>
-			<div class="post">
-				<img src="">
-				<div class="post-text">
-				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis voluptatibus recusandae dolore facilis officiis ex eaque modi suscipit, sequi nesciunt, possimus praesentium nihil. Repellendus reiciendis blanditiis praesentium voluptatem quidem quod, placeat quo recusandae quis dolorem sunt temporibus sequi excepturi mollitia est voluptatum ducimus quas, similique doloribus nobis commodi dolore at qui dicta! Vel, iusto veritatis magnam consequatur autem reprehenderit, asperiores quaerat deserunt adipisci, facilis dignissimos cumque tempore harum ratione rem blanditiis impedit ad pariatur nemo. Sit nostrum corporis magni a aperiam mollitia, maiores laboriosam alias quo praesentium, beatae saepe molestiae debitis cumque quam unde officia sapiente, nisi adipisci tenetur fugiat illo totam! Hic molestiae officiis similique obcaecati. Maxime beatae iste obcaecati repellendus dolore odit expedita veritatis temporibus dolorum, officiis quisquam aspernatur voluptates qui animi consequuntur inventore delectus, accusantium ipsum voluptatem? At sapiente tempora corrupti saepe eum deleniti laboriosam rerum et placeat, nemo, voluptate eaque cum iusto architecto ullam reiciendis eius.
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis voluptatibus recusandae dolore facilis officiis ex eaque modi suscipit, sequi nesciunt, possimus praesentium nihil. Repellendus reiciendis blanditiis praesentium voluptatem quidem quod, placeat quo recusandae quis dolorem sunt temporibus sequi excepturi mollitia est voluptatum ducimus quas, similique doloribus nobis commodi dolore at qui dicta! Vel, iusto veritatis magnam consequatur autem reprehenderit, asperiores quaerat deserunt adipisci, facilis dignissimos cumque tempore harum ratione rem blanditiis impedit ad pariatur nemo. Sit nostrum corporis magni a aperiam mollitia, maiores laboriosam alias quo praesentium, beatae saepe molestiae debitis cumque quam unde officia sapiente, nisi adipisci tenetur fugiat illo totam! Hic molestiae officiis similique obcaecati. Maxime beatae iste obcaecati repellendus dolore odit expedita veritatis temporibus dolorum, officiis quisquam aspernatur voluptates qui animi consequuntur inventore delectus, accusantium ipsum voluptatem? At sapiente tempora corrupti saepe eum deleniti laboriosam rerum et placeat, nemo, voluptate eaque cum iusto architecto ullam reiciendis eius.
 				</div>
 				<div class="about-author">
 
@@ -47,10 +38,30 @@ export function drinksPageHandler(event) {
   mainBody.innerHTML = HTML
   const createBtn = mainBody.querySelector('#create')
   createBtn.addEventListener('click', PostPage)
+  const fileContainer = []
   if (event.target.id) {
     setActiveNavLink(event.target.id)
   } else {
     setActiveNavLink(event.target.parentNode.id)
   }
-  loadContent('drinkPosts')
+  function showContent(selector) {
+    const container = document.getElementById(`${selector}`)
+    // console.log(container)
+    const newPost = document.createElement('div')
+    newPost.classList.add('post')
+    // console.log(newPost)
+    newPost.innerHTML = `
+		<div class="post-header>${newPostFiles[0]}</div>
+		<img src="${newPostFiles[1]}">
+		<div class="post-text">
+			${newPostFiles[2]}
+		</div>
+		<div class="about-author">
+
+		</div>
+		`
+    container.appendChild(newPost)
+  }
+  await loadPostContent('drinkPosts', 'new cool post', fileContainer)
+  console.log(fileContainer)
 }
